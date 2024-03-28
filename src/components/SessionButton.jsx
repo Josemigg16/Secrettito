eb323ca63070b2781b79739ff37bb6f9dcb4a85d
Jose Miguel <josemiggomez16@gmail.com>
2024-03-28 01:37:39 -0400

    added onClick attribute to SessionButton

--- src/components/SessionButton.jsx
+++ src/components/SessionButton.jsx
@@ -1,7 +1,10 @@
-function SessionButton({ className, Icon, children }) {
+function SessionButton({ className, Icon, onClick, children }) {
+
+
 
     return (
         <button
+            onClick={() => onClick()}
             className={`${className} relative border-gray-200 border-2 rounded-full`}>
             <Icon
                 className='absolute -left-[5.7rem] -top-[6.35rem]'
