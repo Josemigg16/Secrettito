f3cd0d0394975f6c08b69ffc8d37e9cb8d144974
Jose Miguel <josemiggomez16@gmail.com>
2024-03-29 22:02:03 -0400

    create settings page

---
+++ src/app/settings/Page.tsx
@@ -0,0 +1,19 @@
+"use client"
+import { useSession } from "next-auth/react"
+import useChooseLanguage from "../hooks/useChooseLang"
+import Header from "@/components/Header"
+
+export default function Page() {
+  const { dict, setLang, ChooseLanguage } = useChooseLanguage({})
+  const { data: session } = useSession()
+  console.log(session)
+
+  return (
+    <main className="">
+      <Header setLang={setLang} ChooseLanguage={ChooseLanguage} />
+      <section className="mt-32">
+        <p>Settings</p>
+      </section>
+    </main>
+  )
+}
