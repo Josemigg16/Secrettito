699d047102f7feddb79e66a1efccd83e047449a1
Jose Miguel <josemiggomez16@gmail.com>
2024-03-31 19:07:52 -0400

    put instagram in coming soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,28 +26,29 @@ export default function Home() {
           <header className="absolute flex justify-end top-0 h-24 w-full">
             <ChooseLanguage className="mr-5" setLang={setLang} />
           </header>
-          <section className="grid h-full md:grid-cols-3">
-            <div className="hidden lg:block"></div>
-            <div className="flex items-center">
-              <article className="mx-5 h-96 w-full rounded-lg bg-white p-5 border-gray-300 border-1 shadow-lg">
+          <section className="grid h-full lg:grid-cols-3">
+            <div className="hidden lg:block col-start-1 col-end-3"></div>
+            <div className="flex justify-center items-center">
+              <article className="mx-5 h-96 min-w-[320px] w-[320px] rounded-lg bg-white p-5 border-gray-300 border-1 shadow-lg">
                 <h2 className="text-center font-bold uppercase text-3xl mt-4 h-[72px]">
                   {dict.registerMessage}
                 </h2>
                 <Divider className="my-8" />
                 <section className="space-y-1">
                   <SessionButton
-                    onClick={() => alert("hello")}
-                    className="w-full h-14 bg-ig-np gradient-hover transition-background"
-                    Icon={Instagram}
-                  >
-                    <p className="text-xl text-gray-100">{dict.signInWithIg}</p>
-                  </SessionButton>
-                  <SessionButton
                     onClick={() => signIn("google")}
                     className="w-full h-14 bg-gg gradient-hover transition-background"
                     Icon={Google}
                   >
                     <p className="text-xl text-gray-100">{dict.signInWithGg}</p>
+                  </SessionButton>
+                  <SessionButton
+                    disabled={true}
+                    onClick={() => alert("hello")}
+                    className="w-full h-14 bg-ig-np opacity-50 transition-background"
+                    Icon={Instagram}
+                  >
+                    <p className="text-xl text-gray-100">{dict.signInWithIg}</p>
                   </SessionButton>
                 </section>
               </article>
