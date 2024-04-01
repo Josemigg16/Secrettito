edc444b76b34e48c72a4b5bd3489b76eac339911
Jose Miguel <josemiggomez16@gmail.com>
2024-04-01 00:11:43 -0400

    add create user funcionality

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,18 +2,55 @@
 import { useSession } from "next-auth/react"
 import useChooseLanguage from "../hooks/useChooseLang"
 import Header from "@/components/Header"
-import { createUser } from "@/libs/createUser"
-import { useEffect } from "react"
+import CreatePost from "@/components/CreatePost"
+import NextUICard from "@/components/NextUICard"
+import { BarlowCondensed } from "@/fonts/fonts"
+import { useEffect, useState } from "react"
+import type { Post } from "@prisma/client"
 
 export default function Page() {
+  const [created, setCreated] = useState(false)
+  const [posts, setPosts] = useState([])
   const { dict, setLang, ChooseLanguage } = useChooseLanguage({})
   const { data: session } = useSession()
 
+  useEffect(() => {
+    if (session) {
+      const getPosts = async () => {
+        const res = await fetch(`/api/users/${session?.user?.email}/posts`)
+        const data = await res.json()
+        setPosts(data)
+      }
+      getPosts()
+    }
+  }, [session, created])
+
   return (
-    <main className="">
-      <Header setLang={setLang} ChooseLanguage={ChooseLanguage} />
-      <section className="mt-32">
-        <p>Article</p>
+    <main
+      className={`${BarlowCondensed.className} h-screen bg-ig overflow-hidden`}
+    >
+      <Header setLang={setLang} ChooseLanguage={ChooseLanguage} session={session} />
+      <section className="mt-6 grid grid-cols-4 px-10">
+        <article className="col-start-1 col-end-4">
+          Mensajes de la publicacion
+        </article>
+        <ul className="px-6 space-y-4 overflow-y-auto max-h-[46em] max-w-[450px]">
+          <CreatePost
+            session={session}
+            created={created}
+            setCreated={setCreated}
+          />
+          {posts.map((post: Post) => (
+            <NextUICard
+              key={post.id}
+              id={post.id}
+              title={post.title}
+              content={post.content}
+              authorId={post.authorId}
+              link={post.link}
+            />
+          ))}
+        </ul>
       </section>
     </main>
   )
