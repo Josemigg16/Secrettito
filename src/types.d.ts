365d2f21ebba5874a06fcf128a10de31a0404d41
Jose Miguel <josemiggomez16@gmail.com>
2024-04-01 03:29:46 -0400

    add more extended post

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,7 +1,14 @@
-import type { Post } from "@prisma/client"
+import type { Post, Message } from "@prisma/client"
 
 export interface ExtendedPost extends Post {
   author: User
   authorId?: string
   link?: string
+}
+
+export interface ExtenderedPost extends Post {
+  author: User
+  authorId?: string
+  link?: string
+  messages: Message[]
 }
