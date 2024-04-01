f2d5b09fe51111befa686e23c4dd140320330a43
Jose Miguel <josemiggomez16@gmail.com>
2024-04-01 03:41:55 -0400

    format

--- src/helpers/randomString.ts
+++ src/helpers/randomString.ts
@@ -1,18 +1,17 @@
 export default function randomString() {
+  let randomValue = ""
 
-    let randomValue = ""
+  for (let i = 0; i < 2; i++) {
+    randomValue += Math.floor(Math.random() * 10)
 
-    for (let i = 0; i < 2; i++) {
-        randomValue += Math.floor(Math.random() * 10)
+    randomValue += String.fromCharCode(97 + Math.floor(Math.random() * 26))
+  }
 
-        randomValue += String.fromCharCode(97 + Math.floor(Math.random() * 26))
-    }
+  for (let i = 0; i < 2; i++) {
+    randomValue += Math.floor(Math.random() * 10)
 
-    for (let i = 0; i < 2; i++) {
-        randomValue += Math.floor(Math.random() * 10)
+    randomValue += String.fromCharCode(65 + Math.floor(Math.random() * 26))
+  }
 
-        randomValue += String.fromCharCode(65 + Math.floor(Math.random() * 26))
-    }
-
-    return randomValue
+  return randomValue
 }
