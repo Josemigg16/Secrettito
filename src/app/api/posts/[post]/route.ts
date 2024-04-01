891e4394c8ac2e70fefedbcff9bd0c71e862e3ad
Jose Miguel <josemiggomez16@gmail.com>
2024-04-01 03:31:26 -0400

    moved out of the function instance of prisma client

--- src/app/api/posts/[post]/route.ts
+++ src/app/api/posts/[post]/route.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from "@prisma/client"
 import { NextRequest, NextResponse } from "next/server"
+const prisma = new PrismaClient()
 
 export async function GET(req: NextRequest) {
-  const prisma = new PrismaClient()
   const link = getParams(req.nextUrl.pathname)
 
   try {
@@ -18,6 +18,28 @@ export async function GET(req: NextRequest) {
     return NextResponse.json(post)
   } catch (error) {
     return NextResponse.json({ message: "Cannot get post" })
+  }
+}
+
+export async function PATCH(req: NextRequest) {
+  const id = getParams(req.nextUrl.pathname)
+  const body = await req.json()
+  try {
+    const post = await prisma.post.update({
+      where: {
+        id,
+      },
+      data: {
+        messages: {
+            create: {
+                content: body.message,
+            }
+        }
+      }
+    })
+    return NextResponse.json('success')
+  } catch (error) {
+    return NextResponse.json({ message: "Cannot update post" })
   }
 }
 
