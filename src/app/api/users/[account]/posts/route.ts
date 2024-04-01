b32600fbd1cd53e9f021fdd23c852bcbf76bfa95
Jose Miguel <josemiggomez16@gmail.com>
2024-04-01 00:11:18 -0400

    route for get user posts

---
+++ src/app/api/users/[account]/posts/route.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client"
+import { NextRequest, NextResponse } from "next/server"
+
+export async function GET(req: NextRequest) {
+  const prisma = new PrismaClient()
+  const email = getParams(req.nextUrl.pathname)
+
+  try {
+    const userPosts = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+      include: {
+        posts: true,
+      }
+    })
+    return NextResponse.json(userPosts?.posts)
+  } catch (error) {
+    return NextResponse.json({ message: "User not deleted" })
+  }
+}
+
+const getParams = (pathname: string) => {
+  const segments = pathname.split("/")
+  return segments[segments.length - 2]
+}
