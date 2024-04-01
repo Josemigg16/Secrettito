57fa0eb30e125c275c0420a909033b61639b50d7
Jose Miguel <josemiggomez16@gmail.com>
2024-04-01 04:47:40 -0400

    rm prisma

--- src/app/api/create-post/route.ts
+++ src/app/api/create-post/route.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client"
 import { NextResponse } from "next/server"
 const prisma = new PrismaClient()
 
-export async function POST(req) {
+export async function POST(req: Request) {
   const body = await req.json()
   try {
     const post = await prisma.post.create({
