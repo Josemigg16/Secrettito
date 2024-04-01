5a0f5469550bce2643889a41deb86e1a275171ac
Jose Miguel <josemiggomez16@gmail.com>
2024-04-01 03:33:00 -0400

    add functionality for go to main post route

--- src/components/postMiniCard.tsx
+++ src/components/postMiniCard.tsx
@@ -1,4 +1,4 @@
-import type { Post } from "@prisma/client"
+import { useRouter } from "next/navigation"
 import {
   Card,
   CardHeader,
@@ -7,29 +7,36 @@ import {
   Divider,
   Link,
 } from "@nextui-org/react"
+import type { Post } from "@prisma/client"
 
-export default function NextUICard({ title, content, link }: Post) {
+export default function PostMiniCard({ title, content, link }: Post) {
+  const router = useRouter()
+
   return (
-    <Card className="max-w-[400px]">
-      <CardHeader className="flex gap-3">
-        <div className="flex flex-col">
-          <p className="text-xl">{title}</p>
-        </div>
-      </CardHeader>
-      <Divider />
-      <CardBody>
-        <p>{content}</p>
-      </CardBody>
-      <Divider />
-      <CardFooter>
-        <Link
-          isExternal
-          showAnchorIcon
-          href="https://github.com/nextui-org/nextui"
-        >
-          Visit source code on GitHub.
-        </Link>
-      </CardFooter>
-    </Card>
+    <button
+    onClick={()=> router.push(`/dashboard/${link}`)}
+    className="w-full block">
+      <Card id="post-card" className="max-w-[450px] cursor-pointer">
+        <CardHeader className="flex gap-3">
+          <div className="flex flex-col">
+            <p className="text-xl">{title}</p>
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <p>{content}</p>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <Link
+            isExternal
+            showAnchorIcon
+            href="https://github.com/nextui-org/nextui"
+          >
+            Visit source code on GitHub.
+          </Link>
+        </CardFooter>
+      </Card>
+    </button>
   )
 }
