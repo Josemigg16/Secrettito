5051c690d1cdcdf52d40fad2f776ce9bc841b8e9
Jose Miguel <josemiggomez16@gmail.com>
2024-04-01 18:40:36 -0400

    show post using queries

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,12 @@
-"use client"
-import { useSession } from "next-auth/react"
-import useChooseLanguage from "../hooks/useChooseLang"
-import Header from "@/components/Header"
-import CreatePost from "@/components/CreatePost"
-import PostList from "@/components/PostList"
-import { BarlowCondensed } from "@/fonts/fonts"
-import { useState } from "react"
+'use client'
+import { useSession } from 'next-auth/react'
+import useChooseLanguage from '../hooks/useChooseLang'
+import Header from '@/components/Header'
+import CreatePost from '@/components/CreatePost'
+import PostList from '@/components/PostList'
+import { BarlowCondensed } from '@/fonts/fonts'
+import { useState } from 'react'
+import ShowPost from '@/components/ShowPost'
 
 export default function Page() {
   const [created, setCreated] = useState(false)
@@ -22,11 +23,7 @@ export default function Page() {
         session={session}
       />
       <section className="mt-6 block h-[75%] grid-cols-8 md:grid md:px-10">
-        <article className="col-start-2 col-end-6 hidden items-center justify-center rounded-xl border-2 border-dashed border-gray-200 md:flex">
-          <h2 className="h-fit select-none text-center text-6xl text-gray-200">
-            Haz click en una publicacion para verla
-          </h2>
-        </article>
+        <ShowPost session={session} />
         <ul className="col-start-6 col-end-9 mx-auto max-w-[450px] space-y-4 overflow-y-auto px-6 md:mx-0">
           <CreatePost
             session={session}
--- src/components/ShowPost.tsx
+++ src/components/ShowPost.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/consistent-type-assertions */
-import { usePathname } from 'next/navigation'
+import { useSearchParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import {
   Card,
@@ -17,9 +17,10 @@ interface Props {
 }
 
 export default function ShowPost({ session }: Props) {
-  const pathname = usePathname()
-  const [, , url] = pathname?.split('/')
+  const searchParams = useSearchParams()
+  const url = searchParams.get('post')
   const [post, setPost] = useState({} as ExtenderedPost)
+  console.log(url)
 
   useEffect(() => {
     if (session) {
@@ -30,41 +31,51 @@ export default function ShowPost({ session }: Props) {
       }
       void getPost()
     }
-  }, [session])
+  }, [session, url])
 
   return (
     <>
-      <Card className="max-w-[450px] cursor-pointer">
-        <CardHeader className="flex gap-3">
-          <div className="flex flex-col">
-            <p className="text-xl">{post.title}</p>
-          </div>
-        </CardHeader>
-        <Divider />
-        <CardBody>
-          <p>{post.content}</p>
-        </CardBody>
-        <CardBody>
-          <h3>Mensajes:</h3>
-          <Divider />
-          {post.messages?.map((message) => (
-            <>
-              <p key={message.id}>{message.content}</p>
+      {!url ? (
+        <article className="col-start-2 col-end-6 hidden items-center justify-center rounded-xl border-2 border-dashed border-gray-200 md:flex">
+          <h2 className="h-fit select-none text-center text-6xl text-gray-200">
+            Haz click en una publicacion para verla
+          </h2>
+        </article>
+      ) : (
+        <article className="col-start-2 col-end-6 hidden items-center justify-center rounded-xl border-2 border-dashed border-gray-200 md:flex">
+          <Card className="max-w-[450px] cursor-pointer">
+            <CardHeader className="flex gap-3">
+              <div className="flex flex-col">
+                <p className="text-xl">{post?.title}</p>
+              </div>
+            </CardHeader>
+            <Divider />
+            <CardBody>
+              <p>{post?.content}</p>
+            </CardBody>
+            <CardBody>
+              <h3>Mensajes:</h3>
               <Divider />
-            </>
-          ))}
-        </CardBody>
-        <Divider />
-        <CardFooter>
-          <Link
-            isExternal
-            showAnchorIcon
-            href="https://github.com/nextui-org/nextui"
-          >
-            Ir a la publicación
-          </Link>
-        </CardFooter>
-      </Card>
+              {post?.messages?.map((message) => (
+                <>
+                  <p key={message?.id}>{message?.content}</p>
+                  <Divider />
+                </>
+              ))}
+            </CardBody>
+            <Divider />
+            <CardFooter>
+              <Link
+                isExternal
+                showAnchorIcon
+                href="https://github.com/nextui-org/nextui"
+              >
+                Ir a la publicación
+              </Link>
+            </CardFooter>
+          </Card>
+        </article>
+      )}
     </>
   )
 }
