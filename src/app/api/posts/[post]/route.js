08c80b2a8f5bc0781dc54de91e4a2dc9d5de960a
Jose Miguel <josemiggomez16@gmail.com>
2024-04-01 05:00:26 -0400

    clear types

--- src/app/api/posts/[post]/route.js
+++ src/app/api/posts/[post]/route.js
@@ -1,8 +1,8 @@
 import { PrismaClient } from "@prisma/client"
-import { NextRequest, NextResponse } from "next/server"
+import { NextResponse } from "next/server"
 const prisma = new PrismaClient()
 
-export async function GET(req: NextRequest) {
+export async function GET(req) {
   const link = getParams(req.nextUrl.pathname)
 
   try {
@@ -21,19 +21,19 @@ export async function GET(req: NextRequest) {
   }
 }
 
-export async function PATCH(req: NextRequest) {
+export async function PATCH(req) {
   const id = getParams(req.nextUrl.pathname)
   const body = await req.json()
   try {
-    const post = await prisma.post.update({
+    await prisma.post.update({
       where: {
         id,
       },
       data: {
         messages: {
-            create: {
-                content: body.message,
-            }
+          create: {
+            content: body.message,
+          }
         }
       }
     })
@@ -43,7 +43,7 @@ export async function PATCH(req: NextRequest) {
   }
 }
 
-const getParams = (pathname: string) => {
+const getParams = (pathname) => {
   const segments = pathname.split("/")
   return segments[segments.length - 1]
 }
