39ba8b87a04b411826d2293cbc6705fe4c4fd940
Jose Miguel <josemiggomez16@gmail.com>
2024-03-31 23:46:17 -0400

    share and toclipboard added icons

---
+++ public/icons/Share.js
@@ -0,0 +1,14 @@
+const SvgComponent = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={800}
+    height={800}
+    className="icon glyph"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path d="M20 21H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h4a1 1 0 0 1 0 2H4v13h16v-6a1 1 0 0 1 2 0v6a2 2 0 0 1-2 2Z" />
+    <path d="m21.62 6.22-5-4a1 1 0 0 0-1.05-.12A1 1 0 0 0 15 3v1.19a9.79 9.79 0 0 0-7 7.65 1 1 0 0 0 .62 1.09A1 1 0 0 0 9 13a1 1 0 0 0 .83-.45c1.17-1.77 3.75-2.31 5.17-2.48V11a1 1 0 0 0 .57.9 1 1 0 0 0 1.05-.12l5-4a1 1 0 0 0 0-1.56Z" />
+  </svg>
+)
+export default SvgComponent
