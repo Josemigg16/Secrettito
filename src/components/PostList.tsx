df250a8566dc19704f527763fc0ca179e43e2a67
Jose Miguel <josemiggomez16@gmail.com>
2024-04-02 18:37:27 -0400

    fix map bug

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -26,9 +26,10 @@ export default function PostList() {
 
   return (
     <>
-      {posts?.map((post: ExtendedPost) => (
-        <PostMiniCard key={post.id} post={post} />
-      ))}
+      {posts &&
+        posts?.map((post: ExtendedPost) => (
+          <PostMiniCard key={post.id} post={post} />
+        ))}
 
       {fetching && (
         <>
