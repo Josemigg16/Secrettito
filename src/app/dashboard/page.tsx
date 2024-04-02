0c8bd029911dd03dc27a767bf8c5fa420e5e8d94
Jose Miguel <josemiggomez16@gmail.com>
2024-04-02 07:09:16 -0400

    clear imports 2

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { useSession } from 'next-auth/react'
-import useChooseLanguage from '../hooks/useChooseLang'
 import { Button, useDisclosure } from '@nextui-org/react'
 import Header from '@/components/Header'
 import CreatePost from '@/components/CreatePost'
@@ -10,32 +9,27 @@ import { BarlowCondensed } from '@/fonts/fonts'
 import { useInfoPostStore } from '@/stores/infoPostStore'
 
 export default function Page() {
-  const { setLang, ChooseLanguage } = useChooseLanguage({})
   const { data: session } = useSession()
   const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure()
   const setPostID = useInfoPostStore((state) => state.setID)
 
   return (
     <main
-      className={`${BarlowCondensed.className} bg-ig h-screen overflow-hidden relative`}
+      className={`${BarlowCondensed.className} bg-ig relative h-screen overflow-hidden`}
     >
-      <Header
-        setLang={setLang}
-        ChooseLanguage={ChooseLanguage}
-        session={session}
-      />
-      <section className="mt-6 block h-[75%] grid-cols-8 md:mt-20 md:grid md:px-10 overflow-y-auto">
+      <Header />
+      <section className="mt-6 block h-[75%] grid-cols-8 overflow-y-auto md:mt-20 md:grid md:px-10">
         <ShowPost session={session} />
         <ul className="col-start-6 col-end-9 mx-auto max-w-[450px] space-y-4 overflow-y-auto px-6 md:mx-0">
           <Button
             variant="light"
-            className="mt-4 h-40 w-full rounded-2xl text-3xl border-2 border-slate-200 text-white hover:scale-105"
+            className="mt-4 h-40 w-full rounded-2xl border-2 border-slate-200 text-3xl text-white hover:scale-105"
             onPress={() => {
               onOpen()
               setPostID('')
             }}
           >
-            <p className='text-wrap'>Crear nueva publicacion</p>
+            <p className="text-wrap">Crear nueva publicacion</p>
           </Button>
           <CreatePost
             isOpen={isOpen}
